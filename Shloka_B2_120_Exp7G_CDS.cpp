725fd10b4355cf6125b7b7fabf87c5dfea121c78
Shloka-Patel <137433503+Shloka-Patel@users.noreply.github.com>
2024-08-07 18:42:50 +0530

    Add files via upload

---
+++ Shloka_B2_120_Exp7G_CDS.cpp
@@ -0,0 +1,19 @@
+// NAME - SHLOKA PATEL 
+// PRM - 23070123120 
+// EXPERIMENT - 7(G) 
+
+#include<iostream> 
+#include<string>
+using namespace std; 
+
+int main() 
+{
+    string a;
+    cout<<"Enter any word: "; 
+    cin>>a;
+    cout<<"Entered string is "<<a<<"."<<"\n";
+    return 0;
+}
+
+// Enter any word: Environment
+// Entered string is Environment.
